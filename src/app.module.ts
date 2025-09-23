e7ec3b677bb4466cad8fe59c3cc9023f3f1046e8
hoangkans <thienkt179@gmail.com>
2025-09-23 11:09:46 +0700

    feat: upadate app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      //'mongodb://localhost:27017/product-crud'
-      'mongodb+srv://thienkt179_db_user:T123456789@test.herh5on.mongodb.net/Testduan',
-    ), // thay URI nếu dùng Atlas
+    ConfigModule.forRoot({
+      isGlobal: true, // để không cần import lại ở module khác
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
     ProductModule,
   ],
 })
